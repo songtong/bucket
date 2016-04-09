bfc7722d776f2fbfb460e9ccb505e9fa042717de
marsqing <q_gu@ctrip.com>
2016-04-09 16:28:49 +0800

    can display cat tx report

---
+++ cat.js
@@ -0,0 +1,62 @@
+function txToChart(txUrl) {
+    $.ajax({
+        url: txUrl,
+    })
+        .done(function(data) {
+            var fs = require('fs');
+            var xml2js = require('xml2js');
+            // data is an XMLDocument object
+            var xmlString = new XMLSerializer().serializeToString(data);
+
+            var parser = new xml2js.Parser();
+            parser.parseString(xmlString, function(err, result) {
+                var nameElement = result.transaction.report[0].machine[0].type[0].name[0];
+
+                var datas = [];
+                for (var i = 0; i < 60; i++) {
+                    datas[i] = parseInt(nameElement.range[i].$.avg);
+                }
+
+
+                $('#container').highcharts({
+                    title: {
+                        text: 'Monthly Average Temperature',
+                        x: -20 //center
+                    },
+                    subtitle: {
+                        text: 'Source: WorldClimate.com',
+                        x: -20
+                    },
+                    /*
+                    xAxis: {
+                        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                    },
+                    */
+                    yAxis: {
+                        title: {
+                            text: 'Temperature (°C)'
+                        },
+                        plotLines: [{
+                            value: 0,
+                            width: 1,
+                            color: '#808080'
+                        }]
+                    },
+                    tooltip: {
+                        valueSuffix: '°C'
+                    },
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'middle',
+                        borderWidth: 0
+                    },
+                    series: [{
+                        name: nameElement.$.id,
+                        data: datas
+                    }]
+                });
+            });
+        });
+}
--- dnd-process.js
+++ dnd-process.js
@@ -1,10 +1,24 @@
 var ipc = require("electron").ipcRenderer;
 ipc.on('something-dropped', function(event, arg) {
+    var url = arg;
     showResult(arg);
 });
 
+function urlType(url) {
+    if(url.indexOf("cat.ctripcorp.com") >= 0) {
+        return "cat";
+    } else if(url.indexOf("zabbix") >= 0) {
+        return "zabbix";
+    } else {
+        return "unknown";
+    }
+}
+
 function showResult(result) {
+    
+    
+    
     var p = document.createElement("div");
-    p.innerText=result;
+    p.innerText = result;
     document.body.appendChild(p);
 }
